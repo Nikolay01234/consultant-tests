d9bc7d4a034c839e6b0ba4e6f0af595d134f450b
Nikolay01234 <prigorodovnikolay@yandex.ru>
2025-12-25 18:25:02 +0400

    Just prints

--- src/test/java/tests/MainPageTest.java
+++ src/test/java/tests/MainPageTest.java
@@ -13,18 +13,23 @@ import static org.testng.Assert.assertTrue;
 @Listeners(AllureTestNg.class)
 public class MainPageTest {
 
-    @BeforeClass
-    public void setUp() {
-        Configuration.browserSize = "1920x1080";
-        Configuration.timeout = 10000;
-    }
+//    @BeforeClass
+//    public void setUp() {
+//        Configuration.browserSize = "1920x1080";
+//        Configuration.timeout = 10000;
+//    }
+//
+//
+//    @Test(dataProvider = "dataProviderMain", dataProviderClass = DataProviderSample.class)
+//    public void testMainPageElements(String element) {
+//        Selenide.open(Sources.MAIN_PAGE);
+//        Selenide.executeJavaScript("window.scrollTo(0, document.body.scrollHeight);");
+//        assertTrue(Selenide.executeJavaScript("return document.body.innerHTML").toString().contains(element),
+//                "Элемент не найден: " + element);
+//    }
 
-
-    @Test(dataProvider = "dataProviderMain", dataProviderClass = DataProviderSample.class)
+    @Test
     public void testMainPageElements(String element) {
-        Selenide.open(Sources.MAIN_PAGE);
-        Selenide.executeJavaScript("window.scrollTo(0, document.body.scrollHeight);");
-        assertTrue(Selenide.executeJavaScript("return document.body.innerHTML").toString().contains(element),
-                "Элемент не найден: " + element);
+        System.out.println("testMainPageElements");
     }
 }
